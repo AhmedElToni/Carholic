0281de0492eb64c4f7cad7d3bf0391feab508f8d
ahmedeltoni <ahmedeltoni99@gmail.com>
2024-07-10 21:52:50 +0300

    Initial Commit

---
+++ Carholic/CarholicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarholicApp.swift
+//  Carholic
+//
+//  Created by Ahmed El Toni on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarholicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
